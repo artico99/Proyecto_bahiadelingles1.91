2df2dc2d19e6ee4994fee54747c0b6c2fb3675f2
Jean <jaxlol78@gmail.com>
2021-12-04 17:18:23 -0300

    hi

--- app/src/main/java/com/example/proyecto_bahiadelingles/Adaptadores/ListaClienteAdapter.java
+++ app/src/main/java/com/example/proyecto_bahiadelingles/Adaptadores/ListaClienteAdapter.java
@@ -15,15 +15,22 @@ import com.example.proyecto_bahiadelingles.Ver_cliente;
 import com.example.proyecto_bahiadelingles.model.Cliente;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListaClienteAdapter extends RecyclerView.Adapter<ListaClienteAdapter.ClienteViewHolder> {
 
     ArrayList<Cliente> listaCliente;
+    ArrayList<Cliente> listaOriginal;
 
     public ListaClienteAdapter(ArrayList<Cliente>listaCliente)
     {
         this.listaCliente = listaCliente;
+        listaOriginal = new ArrayList<>();
+        listaOriginal.addAll(listaCliente);
     }
+
+
 
     @NonNull
     @Override
@@ -39,8 +46,6 @@ public class ListaClienteAdapter extends RecyclerView.Adapter<ListaClienteAdapte
         holder.tvNombreCliente.setText(listaCliente.get(position).getNombre());
         holder.tvApellidoCliente.setText(listaCliente.get(position).getApellido());
         holder.tvRutCliente.setText(listaCliente.get(position).getRut());
-        holder.tvTelefonoCliente.setText(listaCliente.get(position).getTelefono());
-        holder.tvCorreoCliente.setText(listaCliente.get(position).getCorreo());
         holder.tvNumLoftCliente.setText(listaCliente.get(position).getNumeroLoft());
         holder.tvComentarioCliente.setText(listaCliente.get(position).getComentario());
     }
@@ -80,4 +85,35 @@ public class ListaClienteAdapter extends RecyclerView.Adapter<ListaClienteAdapte
             });
         }
     }
+
+    public void filtrado (String txtBuscar)
+    {
+        int longuitud = txtBuscar.length();
+        if(longuitud==0)
+        {
+            listaCliente.clear();
+            listaCliente.addAll(listaOriginal);
+        }
+        else
+            {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N)
+            {
+                List<Cliente> collecion = listaCliente.stream().filter(i -> i.getNombre().toLowerCase().contains(txtBuscar.toLowerCase())).collect(Collectors.toList());
+                listaCliente.clear();
+                listaCliente.addAll(collecion);
+            }
+            else
+                {
+                for (Cliente c:listaOriginal)
+                {
+                    if(c.getNombre().toLowerCase().contains(txtBuscar.toLowerCase()))
+                    {
+                        listaCliente.add(c);
+                    }
+                }
+            }
+        }
+        notifyDataSetChanged();
+    }
+
 }
